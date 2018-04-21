e3942810d9f8984b78bdfb3cadbb8e8475f39ccd
ManarAlameer <37583775+ManarAlameer@users.noreply.github.com>
2018-04-21 14:38:13 +0300

    Update publishe_event.js

--- publishe_event.js
+++ publishe_event.js
@@ -1,13 +1,25 @@
+
+
 var name; 
-var email; 
+                var time; 
+var price; 
+var capacity; 
 var RadioResult; 
 var describtion;
 var check;
+ var date;
+var Month;
+                 var Year;
 function getFirstName() {
     return document.getElementById("name").value;
 }
-function getSecondName() {
-    return document.getElementById("email").value;
+function getCap() {
+    return document.getElementById("capacity").value;
+}
+                 function getTime() {
+    return document.getElementById("time").value;
+}function getPrice() {
+    return document.getElementById("price").value;
 }
 function getButtonRadio()
 {
@@ -15,14 +27,13 @@ function getButtonRadio()
     return document.getElementById("poll1").value; 
     if(document.getElementById("poll2").checked)
     return document.getElementById("poll2").value; 
-    if(document.getElementById("poll3").checked)
-    return document.getElementById("poll3").value; 
+   
 }
 function getCheck()
 {
   if(document.getElementById("check1").checked && document.getElementById("check2").checked)
      { 
-    check=document.getElementById("check1").value+" , "+ document.getElementById("check2").value
+    check=document.getElementById("check1").value+" and "+ document.getElementById("check2").value
     return check;   
     }
       if(document.getElementById("check1").checked)
@@ -35,26 +46,64 @@ function getDesc()
       return document.getElementById("describtion").value;
 
 }
-
+ function getDate()
+            {
+            
+                 date = document.getElementById("date");
+return  date.options[date.selectedIndex].value   
+            }
+function getMonth()
+            {
+            
+                 Month = document.getElementById("Month");
+return  Month.options[Month.selectedIndex].value   
+            }
+                 function getYear()
+            {
+            
+                 Year = document.getElementById("Year");
+return  Year.options[Year.selectedIndex].value   
+            }
 function display() {
     name = getFirstName();
-    email = getSecondName();
+    capacity = getCap();
   RadioResult=getButtonRadio();
   check=getCheck();
   describtion=getDesc();
+    date2=getDate();
+    Month=getMonth();
+    Year=getYear();
+    time=getTime();
+   price=getPrice();
+
+    
+    document.write("Thank you for publishing the following event <br/>");
+
    document.write("Event's Name: <br/>");
      document.write(name);
-   document.write("<br/><br/>Contact via Email:<br/>");
-     document.write(email);
-document.write("<br/><br/>Radio Button result<br/>");
+   document.write("<br/><br/>Total Event Capacity:<br/>");
+     document.write(capacity);
+       document.write("<br/><br/>Time:<br/>");
+     document.write(time);  
+    document.write("<br/><br/>Price:<br/>");
+     document.write(price);
+document.write("<br/><br/>Cancellation Allowd?<br/>");
  document.write(RadioResult);
-   document.write("<br/><br/>Check result: <br/>");
+   document.write("<br/><br/>Gender: <br/>");
      document.write(check);
+    
+       document.write("<br/><br/>Date: <br/>");
+document.write(date2)
+    document.write("/")
+    document.write(Month)
+        document.write("/")
+    document.write(Year)
+
+
+
 document.write("<br/><br/>Describtion<br/>");
  document.write(describtion);
-
     document.getElementById("form").submit();
 
 } 
-
 
